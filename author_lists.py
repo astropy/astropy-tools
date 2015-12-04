cffebcaf7f01ad7fd95b9a4f0e5e66abaf2b7aa7
Erik Tollerud <erik.tollerud@gmail.com>
2015-12-04 13:45:16 -0500

    start on author list script

---
+++ author_lists.py
@@ -0,0 +1,10 @@
+"""
+A script for generating contributor lists for astropy
+
+Note that this requires GitPython (https://gitpython.readthedocs.org).
+"""
+from __future__ import print_function, division
+
+import git
+import argparse
+
