cdf3a6273ad13ed4c269e221edc33a0abb4ba116
Thomas Robitaille <thomas.robitaille@gmail.com>
2017-05-19 22:53:38 +0100

    Add a script to find the next PR number for the changelog

---
+++ next_pr_number.py
@@ -0,0 +1,15 @@
+import sys
+from github import Github
+
+if len(sys.argv) == 2:
+    repository = sys.argv[1]
+elif len(sys.argv) > 2:
+    print("Usage: next_pr_number.py <repository>")
+    sys.exit(1)
+else:
+    repository = 'astropy/astropy'
+
+gh = Github()
+repo = gh.get_repo('astropy/astropy')
+pl = repo.get_issues(sort='created', state='all')
+print("Next PR number: {0}".format(pl.get_page(0)[0].number))
