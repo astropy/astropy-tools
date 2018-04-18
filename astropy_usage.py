7b22eaad23184686dda45e260ce038cc299a0638
Brigitta Sipocz <bsipocz@gmail.com>
2018-04-18 17:15:42 +0100

    Rename variable to something more sensible

--- astropy_usage.py
+++ astropy_usage.py
@@ -21,13 +21,13 @@ if len(sys.argv) > 1:
     print("Loading previous results from {}".format(filename))
     with open(filename, 'rb') as f:
         saved_results = pickle.load(f)
-    step, queried_repos, gh_repo, gh_name, missed_repos = saved_results
+    step, queried_results, gh_repo, gh_name, missed_results = saved_results
 else:
     step = 0
-    queried_repos = 0
+    queried_results = 0
     gh_repo = set()
     gh_name = set()
-    missed_repos = 0
+    missed_results = 0
 
 print("Total number of search results: {}".format(total_repo))
 
@@ -35,8 +35,8 @@ print("Total number of search results: {}".format(total_repo))
 # We need to limit the search by repo size as the results are limited to 1000.
 # (and astropy is imported in 58K+ repositories.
 
-while queried_repos + missed_repos < total_repo:
-    queried_repos_rollback = queried_repos
+while queried_results + missed_results < total_repo:
+    queried_results_rollback = queried_results
     gh_search_result = gh.search_code('{} size:{}..{}'.format(
         search_phrase, step * step_size, (step + 1) * step_size))
     current_total = gh_search_result.totalCount
@@ -44,23 +44,23 @@ while queried_repos + missed_repos < total_repo:
     if current_total > 1000:
         print("Use smaller step_size total count is {} for size {}..{}".format(
             current_total, step * step_size, (step + 1) * step_size))
-        missed_repos += current_total - 1000
+        missed_results += current_total - 1000
 
     try:
         for i in gh_search_result:
             gh_full_name = i.repository.full_name
             gh_repo.add(gh_full_name)
             gh_name.add(gh_full_name.split('/')[-1])
-            queried_repos += 1
+            queried_results += 1
             # This is an ugly hack to work around the API limits
             time.sleep(0.4)
     except GithubException:
         print("Finished search up until step {} for a total of {} repos. "
               "Search failed after the {}th repo.".format(
-                  step, queried_repos_rollback, queried_repos))
-        queried_repos = queried_repos_rollback
+                  step, queried_results_rollback, queried_results))
+        queried_results = queried_results_rollback
         with open('usage_results.pkl', 'wb') as f:
-            results = (step, queried_repos, gh_repo, gh_name, missed_repos)
+            results = (step, queried_results, gh_repo, gh_name, missed_results)
             pickle.dump(results, f)
         raise
 
@@ -68,7 +68,7 @@ while queried_repos + missed_repos < total_repo:
 
     # save partial results at each step
     with open('usage_results.pkl', 'wb') as f:
-        results = (step, queried_repos, gh_repo, gh_name, missed_repos)
+        results = (step, queried_results, gh_repo, gh_name, missed_results)
         pickle.dump(results, f)
 
     time.sleep(30)
