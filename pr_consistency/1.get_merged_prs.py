df83ecc1cf235c5b5aab2bd047e959729a79cad1
Erik Tollerud <erik.tollerud@gmail.com>
2021-06-11 17:00:57 -0400

    fix PR-getter to also get from main

--- pr_consistency/1.get_merged_prs.py
+++ pr_consistency/1.get_merged_prs.py
@@ -12,7 +12,7 @@ from common import get_credentials
 QUERY_TEMPLATE = """
 {{
   repository(owner: "{owner}", name: "{repository}") {{
-    pullRequests(first:100, orderBy: {{direction: ASC, field: CREATED_AT}}, baseRefName: "master", states: MERGED{after}) {{
+    pullRequests(first:100, orderBy: {{direction: ASC, field: CREATED_AT}}, baseRefName: "{basename}", states: MERGED{after}) {{
       edges {{
         node {{
           title
@@ -63,42 +63,40 @@ cursor = None
 pull_requests = {}
 
 try:
+    for basename in ('master', 'main'):
+        print('Searching for PRs into branch', basename)
 
-    while True:
+        entries = True
+        while entries:
+            print('cursor:', cursor)
 
-        print('cursor:', cursor)
+            if cursor is None:
+                after = ''
+            else:
+                after = f', after:"{cursor}"'
 
-        if cursor is None:
-            after = ''
-        else:
-            after = f', after:"{cursor}"'
+            query = QUERY_TEMPLATE.format(owner=OWNER, repository=NAME, after=after, basename=basename)
 
-        query = QUERY_TEMPLATE.format(owner=OWNER, repository=NAME, after=after)
+            request = requests.post('https://api.github.com/graphql', json={'query': query}, headers=headers)
 
-        request = requests.post('https://api.github.com/graphql', json={'query': query}, headers=headers)
+            if request.status_code != 200:
+                raise Exception("Query failed")
 
-        if request.status_code != 200:
-            raise Exception("Query failed")
+            entries = request.json()['data']['repository']['pullRequests']['edges']
 
-        entries = request.json()['data']['repository']['pullRequests']['edges']
+            for entry in entries:
 
-        for entry in entries:
+                pr = entry['node']
+                cursor = entry['cursor']
 
-            pr = entry['node']
-            cursor = entry['cursor']
-
-            pull_requests[str(pr['number'])] = {'milestone': pr['milestone']['title'] if pr['milestone'] else None,
-                                                'title': pr['title'],
-                                                'labels': [edge['node']['name'] for edge in pr['labels']['edges']],
-                                                'merged': pr['mergedAt'].replace('Z', ''),
-                                                'updated': pr['updatedAt'].replace('Z', ''),
-                                                'created': pr['createdAt'].replace('Z', ''),
-                                                'merge_commit': pr['mergeCommit']['oid'] if pr['mergeCommit'] else None}
-
-        if len(entries) < 100:
-            break
+                pull_requests[str(pr['number'])] = {'milestone': pr['milestone']['title'] if pr['milestone'] else None,
+                                                    'title': pr['title'],
+                                                    'labels': [edge['node']['name'] for edge in pr['labels']['edges']],
+                                                    'merged': pr['mergedAt'].replace('Z', ''),
+                                                    'updated': pr['updatedAt'].replace('Z', ''),
+                                                    'created': pr['createdAt'].replace('Z', ''),
+                                                    'merge_commit': pr['mergeCommit']['oid'] if pr['mergeCommit'] else None}
 
 finally:
-
     with open(json_filename, 'w') as f:
         json.dump(pull_requests, f, sort_keys=True, indent=2)
